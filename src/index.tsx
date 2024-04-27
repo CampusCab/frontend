ad3984d270f460c2e2272ded7b9d68f50a35f6fb
Andrés Aparicio <andres.apa.23@gmail.com>
2024-04-27 15:43:02 -0500

    Add sass package and update index.scss file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { PAGES } from './pages/index.tsx'
-import './index.css'
+import './index.scss'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
