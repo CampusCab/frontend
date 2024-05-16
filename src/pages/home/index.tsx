b7eac642125581f929a6d3d99cfd2119bfcfe1fa
Andrés Aparicio <andres.apa.23@gmail.com>
2024-05-16 09:28:15 -0500

    Update home and login pages

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,31 @@
+import './index.scss'
+
+import MainLayout from '../../layouts/main'
+import { UseAuth } from '../../providers/auth'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+
 function Home() {
+  const { isLogged } = UseAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isLogged) navigate('/login', { replace: true })
+  }, [isLogged, navigate])
+
   return (
     <>
-      
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <MainLayout>
+        <section className='welcome'>
+          <h2>Bienvenido</h2>
+          <p>¿A dónde quieres ir ahora?</p>
+          <img
+            src='/assets/banner.svg'
+            alt='CampusCab banner'
+            style={{ position: 'absolute', bottom: '4rem', maxWidth: '430px' }}
+          />
+        </section>
+      </MainLayout>
     </>
   )
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react'
 import LoginForm from '../../containers/loginForm'
 import LoginLayout from '../../layouts/login'
+import { UseAuth } from '../../providers/auth'
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
+  const { isLogged } = UseAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isLogged) navigate('/')
+  }, [isLogged, navigate])
+
   return (
     <>
       <LoginLayout>
